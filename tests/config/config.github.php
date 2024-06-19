51e47db5722044f10cef40901bdef36bf537135f
Yurii Boichenko <yboichenko@connect-i.ch>
2024-06-19 12:28:54 +0200

    Configs update.

--- tests/config/config.github.php
+++ tests/config/config.github.php
@@ -2,9 +2,9 @@
 
 $LRSs = [
     [
-        'endpoint' => env('LRS_ENDPOINT'),
-        'username' => env('LRS_USERNAME'),
-        'password' => env('LRS_PASSWORD'),
+        'endpoint' => getenv('LRS_ENDPOINT'),
+        'username' => getenv('LRS_USERNAME'),
+        'password' => getenv('LRS_PASSWORD'),
         'version'  => '1.0.1'
     ]
 ];
